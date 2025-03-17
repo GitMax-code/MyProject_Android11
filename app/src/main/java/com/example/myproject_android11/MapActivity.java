d3a0f49978514588603f01655f888d640afa9116
Maximilien Dargent <maximilien.dargent@helb-prigogine.be>
2025-03-17 18:23:47 +0100

    ajout fonctionalité trassage polygone

--- app/src/main/java/com/example/myproject_android11/MapActivity.java
+++ app/src/main/java/com/example/myproject_android11/MapActivity.java
@@ -1,5 +1,6 @@
 package com.example.myproject_android11;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polygon;
+import org.osmdroid.views.overlay.Polyline;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +53,9 @@ public class MapActivity extends AppCompatActivity {
 
         // Chargement des données et ajout des marqueurs
         //loadAndAddMarkers();
-        drawRedMarker();
+        //drawRedMarker();
+        //drawPolyline();
+        drawPolygone();
     }
 
     private void drawRedMarker() {
@@ -61,6 +65,51 @@ public class MapActivity extends AppCompatActivity {
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         marker.setIcon(ContextCompat.getDrawable(this, R.drawable.ic_red_marker));
         mapView.getOverlays().add(marker);
+    }
+
+    private void drawPolyline() {
+        ArrayList<GeoPoint> geoPoints = new ArrayList<>();
+        GeoPoint geoPoint1 = new GeoPoint(50.8503, 4.3517);
+        GeoPoint geoPoint2 = new GeoPoint(50.8504, 4.3518);
+        GeoPoint geoPoint3 = new GeoPoint(50.8502, 4.3516);
+        geoPoints.add(geoPoint1);
+        geoPoints.add(geoPoint2);
+        geoPoints.add(geoPoint3);
+        Polyline polyline = new Polyline();
+        polyline.setPoints(geoPoints);
+        polyline.setOnClickListener(new Polyline.OnClickListener() {
+
+            @Override
+            public boolean onClick(Polyline polyline, MapView mapView, GeoPoint eventPos) {
+                Toast.makeText(MapActivity.this, "Polyline clicked", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        mapView.getOverlays().add(polyline);
+    }
+
+
+
+    private void drawPolygone(){
+        ArrayList<GeoPoint> geoPoints = new ArrayList<>();
+        GeoPoint geoPoint1 = new GeoPoint(50.8503, 4.3517);
+        GeoPoint geoPoint2 = new GeoPoint(50.8503, 4.3527);
+        GeoPoint geoPoint3 = new GeoPoint(50.8493, 4.3517);
+        geoPoints.add(geoPoint1);
+        geoPoints.add(geoPoint2);
+        geoPoints.add(geoPoint3);
+        geoPoints.add(geoPoints.get(0));
+        Polygon polygone = new Polygon();
+
+        //FillPaint = contenu du polygone
+        polygone.getFillPaint().setColor(Color.parseColor("#0000FF"));
+        //OutlinePaint = bord du polygone
+        polygone.getOutlinePaint().setColor(Color.parseColor("#FF0000"));
+        polygone.getOutlinePaint().setStrokeWidth(0f);
+        polygone.setPoints(geoPoints);
+        polygone.setTitle("A polygon");
+        mapView.getOverlays().add(polygone);
+
     }
 
     private void loadAndAddMarkers() {
